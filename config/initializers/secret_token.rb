9d46fb4dd0df00dae62ab9dad8a02b56b424ebf6
kashy <h.kashy@icloud.com>
2014-02-09 00:26:47 -0800

    Start
    Project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PreztoApp::Application.config.secret_key_base = 'e6b1789eae03379a176efcc2894e20d0c0770d30245cbabed30c9670a1c2651d44ac4581d13979caf4736ad23e74f72685060754015ec80d00a1f5b0f71ea130'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PreztoApp::Application.config.session_store :cookie_store, key: '_prezto_app_session'
